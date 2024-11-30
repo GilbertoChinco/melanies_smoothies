bdb0697f5c0d8b122dc66b4aaafc6c3409118d28
Gilberto Chincoya <93879683+GilbertoChinco@users.noreply.github.com>
2024-11-30 09:10:41 -0600

    Update streamlit_app.py
    Remove text line

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ st.write(
     """Choose the fruits you want in your custom Smoothie!
     """
 )
-st.write("Hola mundo")
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
